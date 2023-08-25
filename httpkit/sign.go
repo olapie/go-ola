fe052a90f189bcc39d9e758fae3b8443d297a9d5
yxz <yxzbox@gmail.com>
2023-08-25 01:12:54 -0400

    fast commit

---
+++ httpkit/sign.go
@@ -0,0 +1,26 @@
+package httpkit
+
+import (
+	"go.olapie.com/logs"
+	"go.olapie.com/ola/activity"
+	"go.olapie.com/ola/headers"
+	"go.olapie.com/security/base62"
+	"net/http"
+)
+
+func SignRequest(req *http.Request, createAPIKey func(h http.Header)) {
+	a := activity.FromOutgoingContext(req.Context())
+	if a != nil {
+		activity.CopyHeader(req.Header, a)
+	} else {
+		logs.FromContext(req.Context()).Warn("no outgoing context")
+	}
+	if traceID := headers.GetTraceID(req.Header); traceID == "" {
+		if traceID == "" {
+			traceID = base62.NewUUIDString()
+			logs.FromContext(req.Context()).Info("generated trace id " + traceID)
+		}
+		headers.SetTraceID(req.Header, traceID)
+	}
+	createAPIKey(req.Header)
+}
