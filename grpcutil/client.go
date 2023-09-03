63b4d4dad532fcf840d0bcc2b48120ce41c75b8d
yxz <yxzbox@gmail.com>
2023-09-03 18:37:16 -0400

    refactor: grpcutil

---
+++ grpcutil/client.go
@@ -0,0 +1,86 @@
+package grpcutil
+
+import (
+	"context"
+	"crypto/tls"
+	"errors"
+	"time"
+
+	"go.olapie.com/logs"
+	"go.olapie.com/ola/activity"
+	"go.olapie.com/ola/headers"
+	"go.olapie.com/security/base62"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
+	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/grpc/metadata"
+)
+
+func DialTLS(ctx context.Context, server string, options ...grpc.DialOption) (cc *grpc.ClientConn, err error) {
+	options = append(options, grpc.WithTransportCredentials(credentials.NewTLS(&tls.Config{})))
+	return dial(ctx, server, options...)
+}
+
+func DialWithClientCert(ctx context.Context, server string, cert []byte, options ...grpc.DialOption) (cc *grpc.ClientConn, err error) {
+	config := &tls.Config{
+		Certificates: []tls.Certificate{
+			{
+				Certificate: [][]byte{cert},
+			},
+		},
+		ClientAuth: tls.RequireAndVerifyClientCert,
+	}
+	options = append(options, grpc.WithTransportCredentials(credentials.NewTLS(config)))
+	return dial(ctx, server, options...)
+}
+
+func Dial(ctx context.Context, server string, options ...grpc.DialOption) (cc *grpc.ClientConn, err error) {
+	options = append(options, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	return dial(ctx, server, options...)
+}
+
+// WithSigner set trace id, api key and other properties in metadata
+func WithSigner(createAPIKey func(md metadata.MD)) grpc.DialOption {
+	return grpc.WithUnaryInterceptor(func(ctx context.Context, method string, req, reply interface{}, cc *grpc.ClientConn, invoker grpc.UnaryInvoker, opts ...grpc.CallOption) error {
+		ctx = signClientContext(ctx, createAPIKey)
+		return invoker(ctx, method, req, reply, cc, opts...)
+	})
+}
+
+func dial(ctx context.Context, server string, options ...grpc.DialOption) (cc *grpc.ClientConn, err error) {
+	for i := 0; i < 3; i++ {
+		cc, err = grpc.DialContext(ctx, server, options...)
+		if err == nil {
+			return cc, nil
+		}
+
+		if errors.Is(err, context.Canceled) || errors.Is(err, context.DeadlineExceeded) {
+			return nil, err
+		}
+		time.Sleep(time.Second)
+	}
+	return nil, err
+}
+
+func signClientContext(ctx context.Context, createAPIKey func(md metadata.MD)) context.Context {
+	md, ok := metadata.FromOutgoingContext(ctx)
+	if !ok {
+		md = make(metadata.MD)
+	}
+
+	a := activity.FromOutgoingContext(ctx)
+	if a != nil {
+		activity.CopyHeader(md, a)
+	} else {
+		logs.FromContext(ctx).Warn("no outgoing context")
+	}
+	if traceID := headers.GetTraceID(md); traceID == "" {
+		if traceID == "" {
+			traceID = base62.NewUUIDString()
+			logs.FromContext(ctx).Info("generated trace id " + traceID)
+		}
+		headers.SetTraceID(md, traceID)
+	}
+	createAPIKey(md)
+	return metadata.NewOutgoingContext(ctx, md)
+}
---
+++ grpcutil/server.go
@@ -0,0 +1,86 @@
+package grpcutil
+
+import (
+	"context"
+	"log/slog"
+	"reflect"
+	"time"
+
+	"go.olapie.com/logs"
+	"go.olapie.com/ola/activity"
+	"go.olapie.com/ola/errorutil"
+	"go.olapie.com/ola/types"
+	"go.olapie.com/security/base62"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/metadata"
+	"google.golang.org/grpc/status"
+)
+
+func ServerStart(ctx context.Context,
+	info *grpc.UnaryServerInfo,
+	verifyAPIKey func(ctx context.Context, md metadata.MD) bool,
+	authenticate func(ctx context.Context, md metadata.MD) types.UserID) (context.Context, error) {
+	md, ok := metadata.FromIncomingContext(ctx)
+	if !ok {
+		return ctx, status.Error(codes.InvalidArgument, "failed reading request metadata")
+	}
+
+	a := activity.New(info.FullMethod, md)
+	ctx = activity.NewIncomingContext(ctx, a)
+	traceID := a.GetTraceID()
+	if traceID == "" {
+		traceID = base62.NewUUIDString()
+		a.SetTraceID(traceID)
+	}
+	logger := logs.FromContext(ctx).With(slog.String("trace_id", traceID))
+	ctx = logs.NewContext(ctx, logger)
+	fields := make([]any, 0, len(md)+1)
+	fields = append(fields, slog.String("full_method", info.FullMethod))
+	for k, v := range md {
+		if len(v) == 0 {
+			continue
+		}
+		fields = append(fields, slog.String(k, v[0]))
+	}
+	logger.Info("START", fields...)
+
+	if !verifyAPIKey(ctx, md) {
+		attrs := make([]slog.Attr, 0, len(md))
+		for key, val := range md {
+			if len(val) > 0 {
+				attrs = append(attrs, slog.String(key, val[0]))
+			}
+		}
+		logger.Error("invalid api key", md)
+		return ctx, status.Error(codes.InvalidArgument, "failed verifying")
+	}
+
+	uid := authenticate(ctx, md)
+	if uid != nil {
+		a.SetUserID(uid)
+		logger.Info("authenticated", slog.Any("uid", uid.Value()))
+	}
+	return ctx, nil
+}
+
+func ServerFinish(resp any, err error, logger *slog.Logger, startAt time.Time) (any, error) {
+	if err == nil {
+		logger.Info("END", slog.Duration("cost", time.Now().Sub(startAt)))
+		return resp, nil
+	}
+
+	if reflect.TypeOf(err) == statusErrorType {
+		logger.Error("END", logs.Err(err))
+		return nil, err
+	}
+
+	if s := errorutil.GetCode(err); s >= 100 && s < 600 {
+		code := HTTPStatusToCode(s)
+		logger.Info("END", slog.Int("status", s), slog.Int("code", int(code)), logs.Err(err))
+		return nil, status.Error(code, err.Error())
+	}
+	logger.Error("END", logs.Err(err))
+
+	return nil, err
+}
